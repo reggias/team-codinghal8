68d6970fc9f0276f35dfc585cd7dd08200b96e74
go-tiger <inter_tiger_@naver.com>
2023-01-04 10:55:52 +0900

    add migration review

---
+++ models/review.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class review extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  review.init({
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    star: DataTypes.INTEGER,
+    content: DataTypes.STRING(500)
+  }, {
+    sequelize,
+    modelName: 'review',
+  });
+  return review;
+};
